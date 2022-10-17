3720f5a1c7a87a075b01411c47eaba0c0fcc8b4c
Harendra Shakya <harendrashakya2004@gmail.com>
2022-10-17 23:18:17 +0530

    fix: bugs

--- backend/contracts/P2P.sol
+++ backend/contracts/P2P.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract P2P is ReentrancyGuard {
-
     bytes4 private constant T_SELECTOR = bytes4(keccak256(bytes("transfer(address,uint256)")));
     bytes4 private constant TF_SELECTOR =
         bytes4(keccak256(bytes("transferFrom(address,address,uint256)")));
@@ -44,10 +43,9 @@ contract P2P is ReentrancyGuard {
 
     modifier notListed(
         address _fromToken,
-        address _toToken,
-        address _seller
+        address _toToken
     ) {
-        Listing memory listing = listings[_seller][_fromToken][_toToken];
+        Listing memory listing = listings[msg.sender][_fromToken][_toToken];
         require(listing.price == 0, "P2P: Already listed");
         _;
     }
@@ -58,18 +56,23 @@ contract P2P is ReentrancyGuard {
         address _seller
     ) {
         Listing memory listing = listings[_seller][_fromToken][_toToken];
-        require(listing.price > 0, "P2P: Not listed");
+        require(listing.seller != address(0), "P2P: Not listed");
+        _;
+    }
+
+    modifier onlyOwner(address _fromToken, address _toToken) {
+        Listing memory listing = listings[msg.sender][_fromToken][_toToken];
+        require(msg.sender == listing.seller || msg.sender == address(this), "P2P: Not owner");
         _;
     }
 
     modifier isEnoughToken(
         uint256 _price,
         uint256 _amount,
-        address _fromToken,
-        address _seller
+        address _fromToken
     ) {
         require(_price > 0 && _amount > 0, "P2P: Invalid Price");
-        require(IERC20(_fromToken).balanceOf(_seller) >= _amount, "P2P: Not have enough tokens");
+        require(IERC20(_fromToken).balanceOf(msg.sender) >= _amount, "P2P: Not have enough tokens");
         _;
     }
 
@@ -99,14 +102,26 @@ contract P2P is ReentrancyGuard {
     }
 
     function updateListing(
+        address _seller,
         address _fromToken,
         address _toToken,
         uint256 _price,
         uint256 _amount,
         uint256 _limit
-    ) external isEnoughToken(_price, _amount, _fromToken, msg.sender) {
-        listings[msg.sender][_fromToken][_toToken] = Listing(_price, _amount, _limit, msg.sender);
-        emit ListToken(msg.sender, _fromToken, _toToken, _price, _amount, _limit);
+    ) public isListed(_fromToken, _toToken, _seller) onlyOwner(_fromToken, _toToken) {
+        require(_price > 0, "P2P: Invalid Price");
+
+        if (_amount > IERC20(_fromToken).balanceOf(_seller) || _amount == 0) {
+            delete listings[_seller][_fromToken][_toToken];
+        } else {
+            listings[_seller][_fromToken][_toToken] = Listing(
+                _price,
+                _amount,
+                _limit,
+                _seller
+            );
+        }
+        emit ListToken(_seller, _fromToken, _toToken, _price, _amount, _limit);
     }
 
     function listToken(
@@ -117,8 +132,8 @@ contract P2P is ReentrancyGuard {
         uint256 _limit
     )
         external
-        notListed(_fromToken, _toToken, msg.sender)
-        isEnoughToken(_price, _amount, _fromToken, msg.sender)
+        notListed(_fromToken, _toToken)
+        isEnoughToken(_price, _amount, _fromToken)
     {
         listings[msg.sender][_fromToken][_toToken] = Listing(_price, _amount, _limit, msg.sender); // from and to are tokens
         emit ListToken(msg.sender, _fromToken, _toToken, _price, _amount, _limit);
@@ -131,7 +146,7 @@ contract P2P is ReentrancyGuard {
         uint256 _amount
     ) external isListed(_fromToken, _toToken, _seller) nonReentrant {
         Listing memory listing = listings[_seller][_toToken][_fromToken];
-        require(listing.limit >= _amount && _amount <= listing.amount, "P2P: Out of limit");
+        require(_amount >= listing.limit  && _amount <= listing.amount, "P2P: Out of limit");
 
         // `seller from` = `buyer to` and vice versa
         _safeTranferFrom(_fromToken, msg.sender, address(this), listing.price * _amount); // buyer -> contract
@@ -142,18 +157,24 @@ contract P2P is ReentrancyGuard {
 
         _safeTranfer(_toToken, msg.sender, amount);
         _safeTranfer(_fromToken, _seller, amount2);
+
+        updateListing(_seller, _fromToken, _toToken, listing.price, _amount, listing.limit);
         emit BuyToken(msg.sender, _fromToken, _seller, _toToken, _amount, amount2);
     }
 
-    function cancelListing(
-        address _fromToken,
-        address _toToken
-    ) external isListed(_fromToken, _toToken, msg.sender) {
+    function cancelListing(address _fromToken, address _toToken)
+        external
+        isListed(_fromToken, _toToken, msg.sender)
+    {
         delete listings[msg.sender][_fromToken][_toToken];
         emit CancelListing(msg.sender, _fromToken, _toToken);
     }
 
-    function getListing(address _seller, address _fromToken, address _toToken) external view returns (Listing memory) {
+    function getListing(
+        address _seller,
+        address _fromToken,
+        address _toToken
+    ) external view returns (Listing memory) {
         return listings[_seller][_fromToken][_toToken];
     }
 }
